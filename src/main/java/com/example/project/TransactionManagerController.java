5efc649c8ab95d5916865c2e4881c60a1bb346cb
Khushi <ranpurakhushi@gmail.com>
2023-10-31 21:41:00 -0400

    some scene building stuff

--- src/main/java/com/example/project/TransactionManagerController.java
+++ src/main/java/com/example/project/TransactionManagerController.java
@@ -1,6 +1,7 @@
 package com.example.project;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 public class TransactionManagerController
@@ -13,4 +14,9 @@ public class TransactionManagerController
     {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    //make combo box for account type
+    ComboBox<String> cmb = new ComboBox<>();
+
+
 }
