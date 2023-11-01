5fb985321aad1b48572a92a789cef38cf86ea377
Khushi <ranpurakhushi@gmail.com>
2023-11-01 00:53:24 -0400

    little additions

--- src/main/java/com/example/project/TransactionManagerController.java
+++ src/main/java/com/example/project/TransactionManagerController.java
@@ -3,11 +3,29 @@ package com.example.project;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import java.io.File;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.scene.control.Alert.AlertType;
 
 public class TransactionManagerController
 {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private TextField firstName, lastName;
 
     @FXML
     protected void onHelloButtonClick()
